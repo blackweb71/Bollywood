adb1cbd92b86039a130bf871362ecb639badd43f
MD Rajib <83833885+blackweb71@users.noreply.github.com>
2021-11-18 12:29:37 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+x = document.querySelector("title");
+a = document.querySelector(".but1");
+b = document.querySelector(".but2");
+c = document.querySelector(".but3");
+d = document.querySelector(".but4");
+e = document.querySelector(".but5");
+
+x.innerHTML="Villain";
+a.innerHTML="Kabir Singh";
+b.innerHTML="Sanak";
+c.innerHTML="Chehre";
+d.innerHTML="Jai Bhim";
+e.innerHTML="Sooryavanshi";
+
