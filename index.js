579d06d15c514cfa8b78b76c72c8b5e9529d9871
MD Rajib <83833885+blackweb71@users.noreply.github.com>
2021-11-18 13:09:06 +0600

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ b = document.querySelector(".but2");
 c = document.querySelector(".but3");
 d = document.querySelector(".but4");
 e = document.querySelector(".but5");
+f = document.querySelector(".but6");
 
 x.innerHTML="Villain";
 a.innerHTML="Kabir Singh";
@@ -11,4 +12,5 @@ b.innerHTML="Sanak";
 c.innerHTML="Chehre";
 d.innerHTML="Jai Bhim";
 e.innerHTML="Sooryavanshi";
+f.innerHTML="SQUAD";
 
